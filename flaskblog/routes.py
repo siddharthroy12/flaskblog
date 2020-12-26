a55f4e0f3a8af0e8c9cc168d59bb3437a379549e
Siddharth Roy <siddharthroy36912@gmail.com>
2020-12-26 17:14:15 +0530

    Added update profile picture functionality

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,3 +1,6 @@
+import secrets
+import os
+from PIL import Image
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import login_user, current_user, logout_user, login_required
 from flaskblog import app, db, bcrypt
@@ -54,6 +57,16 @@ def login():
                 flash('Password did not match', 'danger')
     return render_template('login.html', form=form)
 
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+    output_size = (125, 125)
+    image = Image.open(form_picture)
+    image.thumbnail(output_size)
+    image.save(picture_path)
+    return picture_fn
 
 @app.route("/profile", methods=['GET', 'POST'])
 @login_required
@@ -61,6 +74,8 @@ def profile():
     form = UpdateProfileForm()
     image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
     if form.validate_on_submit():
+        if form.picture.data:
+            current_user.image_file = save_picture(form.picture.data)
         current_user.username = form.username.data
         current_user.email = form.email.data
         db.session.commit()
