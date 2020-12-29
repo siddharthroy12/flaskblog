487c72f4d5ca11c518e78bd32c623c441a6eb6e9
Siddharth Roy <siddharthroy36912@gmail.com>
2020-12-29 08:14:30 +0530

    Fixed default picture url

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -11,7 +11,7 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True, nullable=False)
     email = db.Column(db.String(120), nullable=False)
-    image_file = db.Column(db.String(120), nullable=False, default='static/profile_pics/default.jpg')
+    image_file = db.Column(db.String(120), nullable=False, default='/static/profile_pics/default.jpg')
     password = db.Column(db.String(120), nullable=False)
     posts = db.relationship('Post', backref='author', lazy=True)
 
